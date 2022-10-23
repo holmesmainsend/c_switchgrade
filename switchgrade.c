baeacb9cb3213d2e789e9ac820ee45056e837bd0
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:26:22 -0400

    added switch statement

--- switchgrade.c
+++ switchgrade.c
@@ -2,8 +2,27 @@
 #include <stdio.h>
 
 int main() {
+    char grade = 'A';
 
-    
+    switch(grade) {
+        case 'A':
+            printf("You aced it!\n");
+            break;
+        case 'B':
+            printf("You did decently!\n");
+            break;
+        case 'C':
+            printf("You did poorly!\n");
+            break;
+        case 'D':
+            printf("You did very poorly!\n");
+            break;
+        case 'F':
+            printf("You failed!\n");
+            break;
+        default:
+            printf("Invalid grade\n");
+    }
 
     return 0;
 }
